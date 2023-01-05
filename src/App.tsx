de6e9f4e74ec7db08c6c0725709d2bb404626f4e
Rafael Alves <devrafaelalves@gmail.com>
2023-01-04 22:38:52 -0300

    testando-api

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import axios from 'axios';
+import {useFormik} from 'formik';
+import { Anime, AnimeList } from './types/AnimeList';
 
-function App() {
+
+
+
+
+
+function App(){
+
+
+  const [currentAnime, setCurrentAnime] = React.useState<Anime>(
+    
+      {
+        attributes: {
+          canonicalTitle: '',
+          episodeCount: 0,
+          posterImage: {
+            medium: '',
+            tiny: '',
+            large: '',
+            original: '',
+          },
+        },
+      },
+    
+  );
+
+  const inputRef = useRef<HTMLInputElement>(null);
+ 
+    function formatSearch(e: React.FormEvent<HTMLFormElement>){
+      e.preventDefault();
+      
+      let name = inputRef.current?.value ?? '';
+
+      name = name?.toLowerCase();
+
+
+
+      const newStr = name.replace(/ /g, '-');
+
+      searchAnime(newStr);
+    }
+
+    const searchAnime = async (name: string) => {
+      const res = await axios.get(`https://kitsu.io/api/edge/anime?filter[text]=${name}`);
+      const { data } = res;
+      console.log(data.data);
+      setCurrentAnime(data.data[0]);
+
+    };
+ 
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
+      <form id='form' onSubmit={e => formatSearch(e)}>
+
+      <input type="text" ref={inputRef} id='name' name='name'/>
+      <input type="submit" form='form' value="Buscar" />
+      </form>
+
+
+      <div>
+        <p>{currentAnime.attributes.canonicalTitle}</p>
+
+        <img src={currentAnime.attributes.posterImage.medium} alt="" />
+        
+      </div>
+     
+     
     </div>
   );
-}
+};
 
 export default App;
+
---
+++ src/types/AnimeList.ts
@@ -0,0 +1,17 @@
+
+export type Anime = {
+    attributes: {
+        canonicalTitle: string
+        episodeCount: number
+        posterImage: {
+            medium: string
+            tiny: string
+            large: string
+            original: string
+        }
+    }
+}
+
+export type AnimeList = {
+    data: [Anime]
+}
