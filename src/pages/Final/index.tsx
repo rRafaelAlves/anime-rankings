3f16c9593627f84a9d0875cc5b201c9538dec10f
Rafael Alves <devrafaelalves@gmail.com>
2023-01-27 18:09:53 -0300

    conventional commits

--- src/pages/Final/index.tsx
+++ src/pages/Final/index.tsx
@@ -3,32 +3,150 @@ import { useNavigate } from 'react-router-dom';
 import Theme from '../../components/Theme';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import { FormActions, useForm } from '../../contexts/ProgramContext';
+import * as C from './styles';
 
 
 
 
 
-
-function Final(){
+function Final() {
     const { state, dispatch } = useForm();
-   const Navigate = useNavigate();
+    const Navigate = useNavigate();
 
-   const handleBackStep = () =>{
-    Navigate('/bestVillan');
- 
-    dispatch({
-     type: FormActions.setCurrentStep,
-     payload: state.currentStep - 1
- })
-  
- }
-    return( 
+    const handleBackStep = () => {
+        Navigate('/bestVillan');
+
+        dispatch({
+            type: FormActions.setCurrentStep,
+            payload: state.currentStep - 1
+        })
+
+    }
+    return (
         <Theme>
 
-            <div>Teste</div>
-         <NavigateBeforeIcon onClick={()=> handleBackStep()} style={{position: 'absolute', top: '44vh', left: 90, fontSize: 50, cursor: 'pointer'}} />
+            <C.Container>
+
+                <C.ContainerFav>
+
+
+
+                    <h4>Melhor História: </h4>
+
+                    <C.ContainerAnime>
+                        <p>1° {state.ListBestHistory[1].attributes.canonicalTitle}</p>
+                        <C.Poster src={state.ListBestHistory[1].attributes.posterImage.original} />
+                    </C.ContainerAnime>
+                    <C.ContainerAnime>
+                        <p>2° {state.ListBestHistory[2].attributes.canonicalTitle}</p>
+                        <C.Poster src={state.ListBestHistory[2].attributes.posterImage.original} />
+                    </C.ContainerAnime>
+                    <C.ContainerAnime>
+                        <p>3° {state.ListBestHistory[0].attributes.canonicalTitle}</p>
+                        <C.Poster src={state.ListBestHistory[0].attributes.posterImage.original} />
+                    </C.ContainerAnime>
+
+
+                </C.ContainerFav>
+
+                <C.ContainerFav>
+
+
+
+                    <h4>Melhor Trilha Sonora: </h4>
+
+                    <C.ContainerAnime>
+                        <p>1° {state.ListBestSoundTrack[1].attributes.canonicalTitle}</p>
+                        <C.Poster src={state.ListBestSoundTrack[1].attributes.posterImage.original} />
+                    </C.ContainerAnime>
+                    <C.ContainerAnime>
+                        <p>2° {state.ListBestSoundTrack[2].attributes.canonicalTitle}</p>
+                        <C.Poster src={state.ListBestSoundTrack[2].attributes.posterImage.original} />
+                    </C.ContainerAnime>
+                    <C.ContainerAnime>
+                        <p>3° {state.ListBestSoundTrack[0].attributes.canonicalTitle}</p>
+                        <C.Poster src={state.ListBestSoundTrack[0].attributes.posterImage.original} />
+                    </C.ContainerAnime>
+
+
+                </C.ContainerFav>
+
+                <C.ContainerFav>
+
+
+
+                    <h4>Melhor Animação: </h4>
+
+                    <C.ContainerAnime>
+                        <p>1° {state.ListBestAnimation[1].attributes.canonicalTitle}</p>
+                        <C.Poster src={state.ListBestAnimation[1].attributes.posterImage.original} />
+                    </C.ContainerAnime>
+                    <C.ContainerAnime>
+                        <p>2° {state.ListBestAnimation[2].attributes.canonicalTitle}</p>
+                        <C.Poster src={state.ListBestAnimation[2].attributes.posterImage.original} />
+                    </C.ContainerAnime>
+                    <C.ContainerAnime>
+                        <p>3° {state.ListBestAnimation[0].attributes.canonicalTitle}</p>
+                        <C.Poster src={state.ListBestAnimation[0].attributes.posterImage.original} />
+                    </C.ContainerAnime>
+
+
+                </C.ContainerFav>
+
+                <C.BoxChar>
+
+                <C.ContainerFav>
+
+
+
+
+
+
+
+                    <h4>Mais Fofo:  </h4>
+
+                    <C.ContainerCharacter>
+                        <p>{state.MoreCute.name}</p>
+                        <C.Poster src={state.MoreCute.poster} />
+                    </C.ContainerCharacter>
+
+                </C.ContainerFav>
+                <C.ContainerFav>
+
+
+
+                    <h4>Melhor Protagonista:  </h4>
+
+                    <C.ContainerCharacter>
+                        <p>{state.BestProtagonist.name}</p>
+                        <C.Poster src={state.BestProtagonist.poster} />
+                    </C.ContainerCharacter>
+
+                </C.ContainerFav>
+                <C.ContainerFav>
+
+
+
+                    <h4>Melhor Vilão:  </h4>
+
+                    <C.ContainerCharacter>
+                        <p>{state.BestVilan.name}</p>
+                        <C.Poster src={state.BestVilan.poster} />
+                    </C.ContainerCharacter>
+
+                </C.ContainerFav>
+
+
+
+                </C.BoxChar>
+
+
+            </C.Container>
+
+            <NavigateBeforeIcon onClick={() => handleBackStep()} style={{ position: 'absolute', top: '44vh', left: 90, fontSize: 50, cursor: 'pointer' }} />
+            <C.Rosinante src='https://practicaltyping.com/wp-content/uploads/2019/05/corazon.jpg' />
         </Theme>
-        
+
     )
 }
 
--- src/pages/Final/styles.ts
+++ src/pages/Final/styles.ts
@@ -1,2 +1,52 @@
 import styled from "styled-components";
 
+
+export const Container = styled.div`
+
+display: flex;
+flex-direction: column;
+
+`
+
+export const ContainerFav = styled.div`
+display: flex;
+justify-content: space-around;
+align-items: center;
+margin: 18px 0;
+
+`
+
+export const ContainerAnime = styled.div`
+display: flex;
+flex-direction: column;
+margin: 0 20px;
+`
+
+export const Poster = styled.img`
+width: 90px;
+height: 120px;
+border-radius: 4px;
+`
+
+export const ContainerCharacter = styled.div`
+display: flex;
+flex-direction: column;
+margin: 0 20px
+`
+export const BoxChar = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+
+`
+
+export const Rosinante = styled.img`
+position: absolute;
+
+right: 300px;
+top: 44vh;
+width: 200px;
+height: 220px;
+border-radius: 12px;
+border: 1px solid black;
+`
